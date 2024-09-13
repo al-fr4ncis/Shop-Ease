eb47548db89beb2abca5235809f338629ed03f8f
francis <alfrancis302@gmail.com>
2024-09-13 13:13:52 +0800

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer()
+      ]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
